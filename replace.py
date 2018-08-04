7f581ec3ce0e976940d97fb070c9bd3f3ae382d0
Kailu Guan <kailuguan@Kailu-MacBook-Air.local>
2018-08-04 18:23:05 -0500

    a replace function

--- replace.py
+++ replace.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
-# Write a function replace(s, old, new) that replaces all occurences of old with new in a string s
+
+'''
+Write a function replace(s, old, new) that replaces all occurences of old with new in a string s:
+test(replace('Mississippi', 'i', 'I'), 'MIssIssIppI')
+
+s = 'I love spom!  Spom is my favorite food.  Spom, spom, spom, yum!'
+test(replace(s, 'om', 'am'),
+       'I love spam!  Spam is my favorite food.  Spam, spam, spam, yum!')
+
+test(replace(s, 'o', 'a'),
+       'I lave spam!  Spam is my favarite faad.  Spam, spam, spam, yum!')
+'''
+
+
 
 # this is the correct way: use split and join to replce all occurences of old 
 def replace(s, old, new):
